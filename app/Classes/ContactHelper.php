932690ba7ab5507a5faec26f30d0a050a07eabe4
Jason Bell <jasonb@inmotionhosting.com>
2017-06-23 16:39:39 -0400

    Moving the majority of contact processing to a helper class to slim down the controller code

---
+++ app/Classes/ContactHelper.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App\Classes;
+
+use App\Contact as Contact;
+use Mail;
+
+class ContactHelper {
+
+    /**
+     * Extract the contact request details from the supplied input array and save them to the database for review
+     * Also send the email to the required email address and react on disposition of the email failures. If there were failures
+     * delete the contact request record and return an error code.
+     *
+     * @param array $input
+     * @return number | \App\Contact
+     */
+    public function addContactRequest($input) {
+        $name        = $input['name'];
+        $email       = $input['email'];
+        $userMessage = $input['message'];
+
+        if ($input['phone']) {
+            $phone = $input['phone'];
+        } else {
+            $phone = null;
+        }
+
+        $emailOptions = [
+            'name'        => $name,
+            'email'       => $email,
+            'userMessage' => $userMessage
+        ];
+        if (isset($phone)) {
+            $emailOptions['phone'] = $phone;
+        }
+        $contact = $this->saveNewContact($name, $email, $userMessage, $phone);
+        $status  = $this->sendContactRequestEmail($emailOptions, $contact);
+        return array($contact, $status);
+    }
+
+    /**
+     * Save the database entry for the contact record
+     *
+     * @param string $name
+     * @param string $email
+     * @param string $userMessage
+     * @param string $phone
+     */
+    protected function saveNewContact($name, $email, $userMessage, $phone) {
+        $contact = new Contact();
+        $contact->name = $name;
+        $contact->email = $email;
+        $contact->message = $userMessage;
+        if (isset($phone)) {
+            $contact->phone = $phone;
+        }
+        $contact->save();
+        return $contact;
+    }
+
+    /**
+     * Attempt to send an email using the configured mail transport service. If there are errors, react
+     * accordingly and return an error status code.
+     *
+     * @param array $emailOptions
+     * @param Contact $contact
+     * @return number
+     */
+    protected function sendContactRequestEmail($emailOptions, Contact $contact) {
+        Mail::send('emails.contact', $emailOptions, function($message) {
+            $message->from('contact@example.com', 'Contact Request');
+            $message->subject('Contact request');
+        });
+        if (count(Mail::failures()) > 0) {
+            $status = 500;
+            $contact->delete();
+        } else {
+            $status = 200;
+        }
+        return $status;
+    }
+}
--- app/Http/Controllers/FormController.php
+++ app/Http/Controllers/FormController.php
@@ -3,43 +3,29 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\ContactFormRequest;
-use Mail;
+use App\Classes\ContactHelper as ContactHelper;
 
 class FormController extends Controller
 {
+    /**
+     * Endpoint action for submission of the contact form through AJAX request
+     *
+     * @param ContactFormRequest $request
+     * @return \Illuminate\Http\JsonResponse
+     */
     public function ajaxContactForm(ContactFormRequest $request) {
 
         $input = $request->all();
 
-
-        $name = $input['name'];
-        $email = $input['email'];
-        $userMessage = $input['message'];
-        if ($input['phone']) {
-            $phone = $input['phone'];
-        }
-        $emailOptions = array(
-            'name' => $name,
-            'email' => $email,
-            'userMessage' => $userMessage
-        );
-        if (isset($phone)) {
-            $emailOptions['phone'] = $phone;
-        }
-        // Send mail
-        //Mail::pretend();
-
-        Mail::send('emails.contact', $emailOptions, function($message) {
-            $message->to('5e4a986243-1aba8e@inbox.mailtrap.io', 'Guy Smiley');
-            $message->subject('Contact request');
-        });
+        $contactHelper = new ContactHelper();
+        list($contact, $status) = $contactHelper->addContactRequest($input);
 
         return response()->json([
-            'name' => $name,
-            'email' => $email,
-            'phone' => $phone,
-            'message' => $message,
-            'status' => $status
+            'name'    => $contact->name,
+            'email'   => $contact->email,
+            'phone'   => $contact->phone,
+            'message' => $contact->message,
+            'status'  => $status
         ]);
     }
 }
